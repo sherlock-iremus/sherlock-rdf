6f60c8d6d856f9225580fb053ab5df104add6bdd
BOTTINI Thomas <thomas.bottini@cnrs.fr>
2026-04-01 16:48:15 +0200

    🌴

--- lib/rdf-prefixes.ts
+++ lib/rdf-prefixes.ts
@@ -9,8 +9,7 @@ export const FOAF_BASE: string = 'http://xmlns.com/foaf/0.1/'
 export const HEMEF_BASE: string = 'http://data-iremus.huma-num.fr/ns/hemef#'
 export const DATA_IREMUS_FILES_BASE: string = 'http://data-iremus.huma-num.fr/files/'
 export const DATA_IREMUS_ID_BASE: string = 'http://data-iremus.huma-num.fr/id/'
-export const IREMUS_RESOURCE_BASE: string = 'http://data-iremus.huma-num.fr/id/'
-export const IREMUS_NS_BASE: string = 'http://data-iremus.huma-num.fr/ns/'
+export const IREMUS_NS_BASE: string = 'http://data-iremus.huma-num.fr/ns/sherlock#'
 export const IREMUS_GRAPH_BASE: string = 'http://data-iremus.huma-num.fr/graph/'
 export const MIMO_BASE: string = 'http://www.mimo-db.eu/'
 export const MUSRAD30_BASE: string = 'http://data-iremus.huma-num.fr/ns/musrad30#'
@@ -20,9 +19,11 @@ export const RDFS_BASE: string = 'http://www.w3.org/2000/01/rdf-schema#'
 export const SCHEMAORG_BASE: string = 'http://schema.org/'
 export const SKOS_BASE: string = 'http://www.w3.org/2004/02/skos/core#'
 
-export const E55_TEI_FILE_URI = IREMUS_RESOURCE_BASE + 'f005e36a-4690-4c83-9791-2927f5f823e0'
-export const E55_FORGE_FILE_URI = IREMUS_RESOURCE_BASE + 'f284ed72-139d-4662-9dce-208bf493a45d'
-export const E55_BUSINESS_ID = IREMUS_RESOURCE_BASE + '574ffe9e-525c-42f2-8188-329ba3c7231d'
+export const E55_TEI_FILE_URI = DATA_IREMUS_ID_BASE + 'f005e36a-4690-4c83-9791-2927f5f823e0'
+export const E55_MEI_FILE_URI = DATA_IREMUS_ID_BASE + '12c5795a-d5fd-4730-9c84-833714b6aaef'
+export const E55_FORGE_FILE_URI = DATA_IREMUS_ID_BASE + 'f284ed72-139d-4662-9dce-208bf493a45d'
+export const E55_BUSINESS_ID = DATA_IREMUS_ID_BASE + '574ffe9e-525c-42f2-8188-329ba3c7231d'
+export const SHERLOCK_E55_PROJECT_OVERVIEW_FILE = DATA_IREMUS_ID_BASE + '518e72ac-3471-4483-b633-e07aecfff49c'
 
 export const RDF_PREFIXES = new Map<string, string>([
     [CRM_BASE, 'crm'],
@@ -34,7 +35,7 @@ export const RDF_PREFIXES = new Map<string, string>([
     [FOAF_BASE, 'foaf'],
     [HEMEF_BASE, 'hemef'],
     [IREMUS_GRAPH_BASE, 'iremus-graph'],
-    [IREMUS_RESOURCE_BASE, 'iremus-data'],
+    [DATA_IREMUS_ID_BASE, 'iremus-data'],
     [IREMUS_NS_BASE, 'iremus-ns'],
     [LRMOO_BASE, 'lrmoo'],
     [MIMO_BASE, 'mimo'],
@@ -78,6 +79,9 @@ export function getGraphName(uri: string) {
 }
 
 export function sortBindings(a: Record<string, any>, b: Record<string, any>): number {
+    if (('p' in a) && !('p' in b)) return -1
+    if (!('p' in a) && ('p' in b)) return 1
+    if (!('p' in a) && !('p' in b)) return 0
     if (a['p']['value'] < b['p']['value']) return -1
     if (a['p']['value'] > b['p']['value']) return 1
     return 0
--- lib/sparql-result.ts
+++ lib/sparql-result.ts
@@ -27,8 +27,22 @@ export class SparqlQueryResultObject_Results {
     }
 }
 
+// POSSIBLE KEYS:
+//   p
+//   r
+//   label
+//   property
+//   value
+//   r_type
+//   r_type_type
+//   r_type_type_label
+//   g
 export class SparqlQueryResultObject_Binding {
     [variable: string]: SparqlQueryResultObject_Variable
+}
+
+export class GroupedSparqlQueryResultObject_Binding {
+    [variable: string]: SparqlQueryResultObject_Variable | SparqlQueryResultObject_Variable[]
 }
 
 export class SparqlQueryResultObject_Variable {
@@ -43,4 +57,39 @@ export class SparqlQueryResultObject_Variable {
         this.value = ''
         this.prefixedUri = new PrefixedUri('', '')
     }
+}
+
+export function makeGroupedBindings(bindings: SparqlQueryResultObject_Binding[], groupByVar: string, varToGroup: string[]): GroupedSparqlQueryResultObject_Binding[] {
+    const res: GroupedSparqlQueryResultObject_Binding[] = []
+    const groups: { [key: string]: SparqlQueryResultObject_Binding[] } = {}
+
+    // 1 - Group bindings by groupByVar
+    for (const binding of bindings) {
+        if (!(groupByVar in binding)) continue
+        for (const _ of varToGroup) {
+            if (!(_ in binding)) continue
+        }
+
+        if (!(binding[groupByVar].value in groups)) groups[binding[groupByVar].value] = []
+        groups[binding[groupByVar].value].push(binding)
+    }
+
+    // 2 - Merge varToGroup
+    for (const bindings of Object.values(groups)) {
+        const groupedBinding: GroupedSparqlQueryResultObject_Binding = {}
+        for (const binding of bindings) {
+            for (const sparqlVar in binding) {
+                if (!varToGroup.includes(sparqlVar)) {
+                    groupedBinding[sparqlVar] = binding[sparqlVar]
+                }
+                else {
+                    if (!(sparqlVar in groupedBinding)) groupedBinding[sparqlVar] = [];
+                    (groupedBinding[sparqlVar] as SparqlQueryResultObject_Variable[]).push(binding[sparqlVar])
+                }
+            }
+        }
+        res.push(groupedBinding)
+    }
+
+    return res
 }
