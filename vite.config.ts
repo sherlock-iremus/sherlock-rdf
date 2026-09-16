e6875de5ddc0e0517c2d2198e0eb90de5e05c402
BOTTINI Thomas <thomas.bottini@cnrs.fr>
2026-09-16 16:03:47 +0200

    📡

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,19 @@
-import { defineConfig } from 'vite'
-import { resolve } from 'path'
-import dts from 'vite-plugin-dts'
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
-  plugins: [
-    dts({ include: ['lib'] })
-  ],
-  build: {
-    copyPublicDir: false,
-    lib: {
-      entry: resolve(__dirname, 'lib/main.ts'),
-      formats: ['es'],
-    },
-    rollupOptions: {
-      output: {
-        entryFileNames: '[name].js',
-      }
-    }
-  }
-})
+	plugins: [dts({ include: ["lib"] })],
+	build: {
+		copyPublicDir: false,
+		lib: {
+			entry: resolve(import.meta.dirname, "lib/main.ts"),
+			formats: ["es"],
+		},
+		rollupOptions: {
+			output: {
+				entryFileNames: "[name].js",
+			},
+		},
+	},
+});
